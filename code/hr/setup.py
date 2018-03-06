e97c4e70e85177c3b93477d89b33cfe639aa16d4
Arseny Chernov <arseny.chernov@gmail.com>
2018-03-06 02:33:28 +0000

    Added users CLI script exercise

--- code/hr/setup.py
+++ code/hr/setup.py
@@ -4,15 +4,16 @@ with open('README.rst', 'r') as f:
     readme = f.read()
 
 setup(
-        name='hrtool',
-        version='0.1.0',
-        description='HR tool exercise',
-        long_description=readme,
-        author='Arseny Chernov',
-        author_email='arseny.chernov@gmail.com',
-        packages=find_packages('src'),
-        package_dir={'': 'src'},
-        install_requires=[],
+    name='hr',
+    version='0.1.0',
+    description='Commandline user management utility',
+    long_description=readme,
+    author='Keith Thompson',
+    author_email='keith@linuxacademy.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[],
+    entry_points={
+        'console_scripts': 'hr=hr.cli:main',
+    },
 )
-
-
